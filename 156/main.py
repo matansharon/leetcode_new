4ce3d4fbec39261de6b16a48abd2e4829ab01425
matan <matca2953@gmail.com>
2023-03-18 15:43:02 +0200

    in the middle of 156

---
+++ 156/main.py
@@ -0,0 +1,23 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+import random
+def random_tree(node):
+    if node is None:
+        return None
+    chance= random.random()
+    if chance>0.3:
+        node.left = TreeNode(random.randint(0, 100))
+        random_tree(node.left)
+    if chance<0.7:
+        node.right =TreeNode(random.randint(0, 100))
+        random_tree(node.right)
+    return node
+head=TreeNode(1)
+head.left = TreeNode(2)
+head.right = TreeNode(3)
+
+
+    
