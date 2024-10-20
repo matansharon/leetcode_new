a7d020600016d9603a1ea41ca94b1af9c76ab80b
matansharon <49459070+matansharon@users.noreply.github.com>
2024-10-20 09:10:18 +0000

    updateing app

--- calculator.py
+++ calculator.py
@@ -1,6 +1,25 @@
 import streamlit as st
+st.write("Welcome to the calculator program!")
 
-
-def main():
-    st.write("Hello, world!")
-    st.write("This is a calculator.")
+# def main():
+#     print("Welcome to the calculator program!")
+#     print("Please enter the first number: ")
+#     num1 = float(input())
+#     print("Please enter the second number: ")
+#     num2 = float(input())
+#     print("Please enter the operation you would like to perform: ")
+#     operation = input()
+#     if operation == "+":
+#         print(num1 + num2)
+#     elif operation == "-":
+#         print(num1 - num2)
+#     elif operation == "*":
+#         print(num1 * num2)
+#     elif operation == "/":
+#         print(num1 / num2)
+#     else:
+#         print("Invalid operation")
+#     print("Thank you for using the calculator program!")
+    
+# if __name__ == "__main__":
+#     main()
